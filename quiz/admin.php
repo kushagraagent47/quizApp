e51264eceb5748552b82badcf30166eae2d7c2f4
Kushagra Kumar <kushagraagent47@gmail.com>
2021-01-07 20:25:59 +0530

    Added things

--- quiz/admin.php
+++ quiz/admin.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['admin'])) {
 if (isset($_POST['password']))  {
 	$password = mysqli_real_escape_string($conn , $_POST['password']);
 	$adminpass = '$2y$10$8WkSLFcoaqhJUJoqjg3K8eWixJWswsICf7FTxehKmx8hpmIKYWqju';
-	if (password_verify($password , $adminpass)) {
+	if ($password == $adminpass) {
 		$_SESSION['admin'] = "active";
 		header("Location: adminhome.php");
 	}
